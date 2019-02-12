73d12926a3729c196e971dab9bc466a99d8adaa8
Jorge Candeias <candeias@fastmail.fm>
2019-02-12 15:29:18 +0000

    Added basic binary gap efficient search.

---
+++ src/Quicker/BinaryGapExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quicker
+{
+    public static class BinaryGapExtensions
+    {
+        /// <summary>
+        /// Finds the length of the maximum binary gap in the given number by using an efficient algorithm.
+        /// </summary>
+        /// <param name="value">The number.</param>
+        /// <returns></returns>
+        public static int BinaryGapMaxLengthEfficient(this int value)
+        {
+            // trackers
+            int max_gap = 0;
+            int gap = 0;
+            int mask = 1;
+
+            // shift until the first set bit
+            while (mask != 0 && (mask & value) == 0)
+            {
+                mask <<= 1;
+            }
+
+            // we are either at the first set bit or have run out of bits
+            for (; mask != 0; mask <<= 1)
+            {
+                // check if the current bit is set
+                if ((mask & value) == mask)
+                {
+                    // check if the current gap is greater than the max candidate
+                    if (gap > max_gap)
+                    {
+                        // promote the current gap count to max candidate
+                        max_gap = gap;
+                    }
+
+                    // close the running gap count
+                    gap = 0;
+                }
+                else
+                {
+                    // if zero then increase the running gap count
+                    ++gap;
+                }
+            }
+
+            // at this point we have the greatest gap length or zero
+            return max_gap;
+        }
+    }
+}
---
+++ test/Quicker.Tests/BinaryGapTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Quicker.Tests
+{
+    [TestFixture]
+    public class BinaryGapTests
+    {
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, 0)]
+        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), 0)]
+        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), 0)]
+        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), 0)]
+        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), 0)]
+        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), 30)]
+        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), 24)]
+        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), 16)]
+        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), 8)]
+        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), 8)]
+        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), 5)]
+        public void BinaryGapMaxLengthEfficient_Returns_Expected_Result(int value, int expected)
+        {
+            // act
+            int actual = value.BinaryGapMaxLengthEfficient();
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
