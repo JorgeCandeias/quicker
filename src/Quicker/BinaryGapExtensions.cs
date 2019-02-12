234866531a140106d5e095d971837391e6f73e5d
Jorge Candeias <candeias@fastmail.fm>
2019-02-12 22:05:20 +0000

    Added binary gap algorithm settings.

--- src/Quicker/BinaryGapExtensions.cs
+++ src/Quicker/BinaryGapExtensions.cs
@@ -1,22 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Quicker
 {
+    public enum BinaryGapMethod
+    {
+        /// <summary>
+        /// Use an efficient approach.
+        /// </summary>
+        Efficient = 0,
+
+        /// <summary>
+        /// Use a simple approach.
+        /// Useful for performance comparison only.
+        /// </summary>
+        Simple = -1
+    }
+
+    public enum BinaryGapBit
+    {
+        /// <summary>
+        /// Look for gaps where the bits are unset.
+        /// </summary>
+        Unset = 0,
+
+        /// <summary>
+        /// Look for gaps where the bits are set.
+        /// </summary>
+        Set = 1
+    }
+
     public static class BinaryGapExtensions
     {
+        /// <summary>
+        /// Finds the length of the maximum binary bag in the given number by using the given method.
+        /// </summary>
+        /// <param name="value">The number.</param>
+        /// <param name="method">The algorithm method. Defaults to <see cref="BinaryGapMethod.Efficient"/></param>
+        /// <returns>The length of the maximum binary gap or zero if there is none.</returns>
+        public static int BinaryGapMaxLength(this int value, BinaryGapMethod method = BinaryGapMethod.Efficient, BinaryGapBit gapBit = BinaryGapBit.Unset)
+        {
+            switch (method)
+            {
+                case BinaryGapMethod.Efficient:
+                    return BinaryGapMaxLengthEfficient(value);
+
+                default:
+                    throw new ArgumentException(nameof(method));
+            }
+        }
+
         /// <summary>
         /// Finds the length of the maximum binary gap in the given number by using an efficient algorithm.
         /// </summary>
         /// <param name="value">The number.</param>
         /// <returns></returns>
-        public static int BinaryGapMaxLengthEfficient(this int value)
+        public static int BinaryGapMaxLengthEfficient(this int value, BinaryGapBit bit = BinaryGapBit.Unset)
         {
             // trackers
             int max_gap = 0;
             int gap = 0;
             int mask = 1;
+
+            // if searching for gaps of ones just flip the bits in the search space
+            if (bit == BinaryGapBit.Set)
+            {
+                value = ~value;
+            }
 
             // shift until the first set bit
             while (mask != 0 && (mask & value) == 0)
--- test/Quicker.Tests/BinaryGapTests.cs
+++ test/Quicker.Tests/BinaryGapTests.cs
@@ -5,24 +5,43 @@ namespace Quicker.Tests
     [TestFixture]
     public class BinaryGapTests
     {
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), 0)]
-        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), 0)]
-        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), 0)]
-        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), 30)]
-        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), 24)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), 16)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), 8)]
-        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), 8)]
-        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), 5)]
-        public void BinaryGapMaxLengthEfficient_Returns_Expected_Result(int value, int expected)
+        // test cases for unset bit gap
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Unset, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Unset, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Unset, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Unset, 0)]
+        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Unset, 0)]
+        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Unset, 30)]
+        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 24)]
+        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 16)]
+        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Unset, 8)]
+        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Unset, 8)]
+        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Unset, 5)]
+        [TestCase(unchecked((int)0b1011_0111_0111_1011_1110_1111_110_1111), BinaryGapBit.Unset, 1)]
+
+        // test cases for set bit gap
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Set, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Set, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Set, 0)]
+        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Set, 0)]
+        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Set, 0)]
+        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Set, 0)]
+        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Set, 0)]
+        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Set, 4)]
+        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Set, 4)]
+        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Set, 4)]
+        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Set, 1)]
+        [TestCase(unchecked((int)0b1011_0111_0111_1011_1110_1111_110_1111), BinaryGapBit.Set, 6)]
+        public void BinaryGapMaxLengthEfficient_Returns_Expected_Result(int value, BinaryGapBit bit, int expected)
         {
             // act
-            int actual = value.BinaryGapMaxLengthEfficient();
+            int actual = value.BinaryGapMaxLengthEfficient(bit);
 
             // assert
             Assert.AreEqual(expected, actual);
