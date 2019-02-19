bbe4dc15e36c142573215ffbbdadbb5817287978
Jorge Candeias <candeias@fastmail.fm>
2019-02-19 19:23:20 +0000

    Migrated tests to xunit.

--- test/Quicker.Tests/BinaryGapTests.cs
+++ test/Quicker.Tests/BinaryGapTests.cs
@@ -1,93 +1,96 @@
-﻿using NUnit.Framework;
-using Quicker.Algorithms.BinaryGap;
+﻿using Quicker.Algorithms.BinaryGap;
+using Xunit;
 
 namespace Quicker.Tests
 {
-    [TestFixture]
     public class BinaryGapTests
     {
+        [Theory]
+
         // test cases for unset bit gap
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Unset, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Unset, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Unset, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Unset, 30)]
-        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 24)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 16)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Unset, 8)]
-        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Unset, 8)]
-        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Unset, 5)]
-        [TestCase(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Unset, 1)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Unset, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Unset, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Unset, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Unset, 30)]
+        [InlineData(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 24)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 16)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Unset, 8)]
+        [InlineData(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Unset, 8)]
+        [InlineData(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Unset, 5)]
+        [InlineData(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Unset, 1)]
 
         // test cases for set bit gap
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Set, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Set, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Set, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Set, 4)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Set, 4)]
-        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Set, 4)]
-        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Set, 1)]
-        [TestCase(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Set, 6)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Set, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Set, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Set, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Set, 4)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Set, 4)]
+        [InlineData(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Set, 4)]
+        [InlineData(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Set, 1)]
+        [InlineData(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Set, 6)]
         public void BinaryGapMaxLengthByShifting_Returns_Expected_Result(int value, BinaryGapBit bit, int expected)
         {
             // act
             int actual = BinaryGapAlgorithm.MaxLengthByShifting(value, bit);
 
             // assert
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
 
+        [Theory]
+
         // test cases for unset bit gap
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Unset, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Unset, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Unset, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Unset, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Unset, 30)]
-        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 24)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 16)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Unset, 8)]
-        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Unset, 8)]
-        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Unset, 5)]
-        [TestCase(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Unset, 1)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Unset, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Unset, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Unset, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Unset, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Unset, 30)]
+        [InlineData(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 24)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Unset, 16)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Unset, 8)]
+        [InlineData(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Unset, 8)]
+        [InlineData(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Unset, 5)]
+        [InlineData(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Unset, 1)]
 
         // test cases for set bit gap
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Set, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Set, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Set, 0)]
-        [TestCase(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Set, 0)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Set, 4)]
-        [TestCase(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Set, 4)]
-        [TestCase(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Set, 4)]
-        [TestCase(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Set, 1)]
-        [TestCase(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Set, 6)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0000, BinaryGapBit.Set, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0001, BinaryGapBit.Set, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0011, BinaryGapBit.Set, 0)]
+        [InlineData(0b0000_0000_0000_0000_0000_0000_0000_0111, BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1100_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1110_0000_0000_0000_0000_0000_0000_0000), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1111_1111_1111_1111_1111_1111_1111_1111), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1000_0000_0000_0000_0000_0000_0000_0001), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1111_0000_0000_0000_0000_0000_0000_1111), BinaryGapBit.Set, 0)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_0000_0000_0000_1111), BinaryGapBit.Set, 4)]
+        [InlineData(unchecked((int)0b1111_0000_1111_0000_1111_0000_0000_1111), BinaryGapBit.Set, 4)]
+        [InlineData(unchecked((int)0b1111_0000_0000_1111_0000_1111_0000_1111), BinaryGapBit.Set, 4)]
+        [InlineData(unchecked((int)0b1111_0100_1000_1000_0100_0001_0011_1111), BinaryGapBit.Set, 1)]
+        [InlineData(0b1011_0111_0111_1011_1110_1111_110_1111, BinaryGapBit.Set, 6)]
         public void BinaryGapMaxLengthByIterating_Returns_Expected_Result(int value, BinaryGapBit bit, int expected)
         {
             // act
             int actual = BinaryGapAlgorithm.MaxLengthByIterating(value, bit);
 
             // assert
-            Assert.AreEqual(expected, actual);
+            Assert.Equal(expected, actual);
         }
     }
 }
--- test/Quicker.Tests/PairingTests.cs
+++ test/Quicker.Tests/PairingTests.cs
@@ -1,48 +1,52 @@
-﻿using NUnit.Framework;
-using Quicker.Algorithms.Pairing;
+﻿using Quicker.Algorithms.Pairing;
+using Xunit;
 
 namespace Quicker.Tests
 {
     public class PairingTests
     {
-        [TestCase(new[] { 1, 1 }, null)]
-        [TestCase(new[] { 1, 1, 2 }, 2)]
-        [TestCase(new[] { 1, 1, 2, 2, 3 }, 3)]
-        [TestCase(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
-        [TestCase(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
+        [Theory]
+        [InlineData(new[] { 1, 1 }, null)]
+        [InlineData(new[] { 1, 1, 2 }, 2)]
+        [InlineData(new[] { 1, 1, 2, 2, 3 }, 3)]
+        [InlineData(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
+        [InlineData(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
         public void FindOddOneOutByIterating_Returns_Expected_Result(int[] values, int? expected)
         {
-            Assert.AreEqual(expected, PairingAlgorithm.FindOddOneOutByIterating(values));
+            Assert.Equal(expected, PairingAlgorithm.FindOddOneOutByIterating(values));
         }
 
-        [TestCase(new[] { 1, 1 }, null)]
-        [TestCase(new[] { 1, 1, 2 }, 2)]
-        [TestCase(new[] { 1, 1, 2, 2, 3 }, 3)]
-        [TestCase(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
-        [TestCase(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
+        [Theory]
+        [InlineData(new[] { 1, 1 }, null)]
+        [InlineData(new[] { 1, 1, 2 }, 2)]
+        [InlineData(new[] { 1, 1, 2, 2, 3 }, 3)]
+        [InlineData(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
+        [InlineData(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
         public void FindOddOneOutByHashing_Returns_Expected_Result(int[] values, int? expected)
         {
-            Assert.AreEqual(expected, PairingAlgorithm.FindOddOneOutByHashing(values));
+            Assert.Equal(expected, PairingAlgorithm.FindOddOneOutByHashing(values));
         }
 
-        [TestCase(new[] { 1, 1 }, null)]
-        [TestCase(new[] { 1, 1, 2 }, 2)]
-        [TestCase(new[] { 1, 1, 2, 2, 3 }, 3)]
-        [TestCase(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
-        [TestCase(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
+        [Theory]
+        [InlineData(new[] { 1, 1 }, null)]
+        [InlineData(new[] { 1, 1, 2 }, 2)]
+        [InlineData(new[] { 1, 1, 2, 2, 3 }, 3)]
+        [InlineData(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
+        [InlineData(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
         public void FindOddOneOutByOrdering_Returns_Expected_Result(int[] values, int? expected)
         {
-            Assert.AreEqual(expected, PairingAlgorithm.FindOddOneOutByOrdering(values));
+            Assert.Equal(expected, PairingAlgorithm.FindOddOneOutByOrdering(values));
         }
 
-        [TestCase(new[] { 1, 1 }, null)]
-        [TestCase(new[] { 1, 1, 2 }, 2)]
-        [TestCase(new[] { 1, 1, 2, 2, 3 }, 3)]
-        [TestCase(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
-        [TestCase(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
+        [Theory]
+        [InlineData(new[] { 1, 1 }, null)]
+        [InlineData(new[] { 1, 1, 2 }, 2)]
+        [InlineData(new[] { 1, 1, 2, 2, 3 }, 3)]
+        [InlineData(new[] { 4, 1, 1, 2, 2, 3, 3 }, 4)]
+        [InlineData(new[] { 4, 4, 1, 1, 5, 2, 2, 3, 3 }, 5)]
         public void FindOddOneOutHybrid_Returns_Expected_Result(int[] values, int? expected)
         {
-            Assert.AreEqual(expected, PairingAlgorithm.FindOddOneOutHybrid(values));
+            Assert.Equal(expected, PairingAlgorithm.FindOddOneOutHybrid(values));
         }
     }
 }
