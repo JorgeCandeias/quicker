45e2f21eb412b3fa62183e9fd68fe7b5776790bf
Jorge Candeias <candeias@fastmail.fm>
2019-02-19 10:02:09 +0000

    Cleanup.

--- src/Quicker/Algorithms/BinaryGap/BinaryGapAlgorithmExtensions.cs
+++ src/Quicker/Algorithms/BinaryGap/BinaryGapAlgorithmExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Quicker.Algorithms.BinaryGap
+﻿namespace Quicker.Algorithms.BinaryGap
 {
     /// <summary>
     /// Helper extensions to faciliate fluent code.
@@ -8,24 +6,13 @@ namespace Quicker.Algorithms.BinaryGap
     public static class BinaryGapAlgorithmExtensions
     {
         /// <summary>
-        /// Finds the length of the maximum binary bag in the given number by using the given method.
+        /// Finds the length of the maximum binary bag in the given number.
         /// </summary>
-        /// <param name="value">The number.</param>
-        /// <param name="method">The algorithm method. Defaults to <see cref="BinaryGapMethod.Shift"/></param>
+        /// <param name="value">The number to evaluate.</param>
         /// <returns>The length of the maximum binary gap or zero if there is none.</returns>
-        public static int BinaryGapMaxLength(this int value, BinaryGapMethod method = BinaryGapMethod.Shift, BinaryGapBit gapBit = BinaryGapBit.Unset)
+        public static int BinaryGapMaxLength(this int value, BinaryGapBit gapBit = BinaryGapBit.Unset)
         {
-            switch (method)
-            {
-                case BinaryGapMethod.Shift:
-                    return BinaryGapAlgorithm.MaxLengthByShifting(value, gapBit);
-
-                case BinaryGapMethod.Iterate:
-                    return BinaryGapAlgorithm.MaxLengthByIterating(value, gapBit);
-
-                default:
-                    throw new ArgumentException(nameof(method));
-            }
+            return BinaryGapAlgorithm.MaxLengthByShifting(value, gapBit);
         }
     }
 }
