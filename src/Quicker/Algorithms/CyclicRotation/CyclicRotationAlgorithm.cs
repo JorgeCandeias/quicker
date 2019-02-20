7e297d513ec4b4b7482e1ba31e64221b33a91c3b
Jorge Candeias <candeias@fastmail.fm>
2019-02-20 22:59:43 +0000

    Added CyclicRotationAlgorithm.RotateByArrayCopy.

--- src/Quicker/Algorithms/CyclicRotation/CyclicRotationAlgorithm.cs
+++ src/Quicker/Algorithms/CyclicRotation/CyclicRotationAlgorithm.cs
@@ -23,5 +23,23 @@ namespace Quicker.Algorithms.CyclicRotation
             }
             return result;
         }
+
+        public static T[] RotateByArrayCopy<T>(T[] input, int distance)
+        {
+            // validate
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (distance < 0) throw new ArgumentOutOfRangeException(nameof(distance));
+
+            // special cases
+            var result = new T[input.Length];
+            if (input.Length < 2) return result;
+            if (distance == 0) return result;
+
+            // rotate
+            int diff = distance % input.Length;
+            Array.Copy(input, 0, result, diff, input.Length - diff);
+            Array.Copy(input, input.Length - diff, result, 0, diff);
+            return result;
+        }
     }
 }
--- test/Quicker.Tests/CyclicRotationTests.cs
+++ test/Quicker.Tests/CyclicRotationTests.cs
@@ -14,10 +14,32 @@ namespace Quicker.Tests
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 3, new int[] { 3, 4, 5, 0, 1, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 2, 3, 4, 5, 0, 1 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 5, new int[] { 1, 2, 3, 4, 5, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 6, new int[] { 0, 1, 2, 3, 4, 5 })]
         public void CyclicRotation_RotateByRemainderIndexing_Returns_Expected_Result(int[] input, int distance, int[] expected)
         {
             // act
             var result = CyclicRotationAlgorithm.RotateByRemainderIndexing(input, distance);
+
+            // assert
+            Assert.True(result.SequenceEqual(expected));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 0 }, 0, new int[] { 0 })]
+        [InlineData(new int[] { 0 }, 1, new int[] { 0 })]
+        [InlineData(new int[] { 0, 1 }, 1, new int[] { 1, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 3, new int[] { 3, 4, 5, 0, 1, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 2, 3, 4, 5, 0, 1 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 5, new int[] { 1, 2, 3, 4, 5, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 6, new int[] { 0, 1, 2, 3, 4, 5 })]
+        public void CyclicRotation_RotateByArrayCopy_Returns_Expected_Result(int[] input, int distance, int[] expected)
+        {
+            // act
+            var result = CyclicRotationAlgorithm.RotateByArrayCopy(input, distance);
 
             // assert
             Assert.True(result.SequenceEqual(expected));
