8ce4b6db5dff213c3d96a28fa4365aaa46e3c8f6
Jorge Candeias <candeias@fastmail.fm>
2019-02-20 22:53:36 +0000

    Added CyclicRotationAlgorithm.RotateByRemainderIndexing

---
+++ src/Quicker/Algorithms/CyclicRotation/CyclicRotationAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Quicker.Algorithms.CyclicRotation
+{
+    public static class CyclicRotationAlgorithm
+    {
+        public static T[] RotateByRemainderIndexing<T>(T[] input, int distance)
+        {
+            // validate
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (distance < 0) throw new ArgumentOutOfRangeException(nameof(distance));
+
+            // special cases
+            var result = new T[input.Length];
+            if (input.Length < 2) return result;
+            if (distance == 0) return result;
+
+            // rotate
+            for (int i = 0; i < input.Length; ++i)
+            {
+                int j = (i + distance) % input.Length;
+                result[j] = input[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ test/Quicker.Tests/CyclicRotationTests.cs
@@ -0,0 +1,26 @@
+﻿using Quicker.Algorithms.CyclicRotation;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Quicker.Tests
+{
+    public class CyclicRotationTests
+    {
+        [Theory]
+        [InlineData(new int[] { 0 }, 0, new int[] { 0 })]
+        [InlineData(new int[] { 0 }, 1, new int[] { 0 })]
+        [InlineData(new int[] { 0, 1 }, 1, new int[] { 1, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 3, new int[] { 3, 4, 5, 0, 1, 2 })]
+        public void CyclicRotation_RotateByRemainderIndexing_Returns_Expected_Result(int[] input, int distance, int[] expected)
+        {
+            // act
+            var result = CyclicRotationAlgorithm.RotateByRemainderIndexing(input, distance);
+
+            // assert
+            Assert.True(result.SequenceEqual(expected));
+        }
+    }
+}
