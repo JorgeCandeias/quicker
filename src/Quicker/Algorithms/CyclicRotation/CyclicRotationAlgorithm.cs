e31c485b2bbb471b8d6fe92720db696c94e4adf4
Jorge Candeias <candeias@fastmail.fm>
2019-02-21 07:50:10 +0000

    Added CyclicRotation - WIP

--- src/Quicker/Algorithms/CyclicRotation/CyclicRotationAlgorithm.cs
+++ src/Quicker/Algorithms/CyclicRotation/CyclicRotationAlgorithm.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 namespace Quicker.Algorithms.CyclicRotation
 {
@@ -10,12 +13,8 @@ namespace Quicker.Algorithms.CyclicRotation
             if (input == null) throw new ArgumentNullException(nameof(input));
             if (distance < 0) throw new ArgumentOutOfRangeException(nameof(distance));
 
-            // special cases
-            var result = new T[input.Length];
-            if (input.Length < 2) return result;
-            if (distance == 0) return result;
-
             // rotate
+            var result = new T[input.Length];
             for (int i = 0; i < input.Length; ++i)
             {
                 int j = (i + distance) % input.Length;
@@ -30,15 +29,67 @@ namespace Quicker.Algorithms.CyclicRotation
             if (input == null) throw new ArgumentNullException(nameof(input));
             if (distance < 0) throw new ArgumentOutOfRangeException(nameof(distance));
 
-            // special cases
-            var result = new T[input.Length];
-            if (input.Length < 2) return result;
-            if (distance == 0) return result;
-
             // rotate
+            var result = new T[input.Length];
             int diff = distance % input.Length;
             Array.Copy(input, 0, result, diff, input.Length - diff);
             Array.Copy(input, input.Length - diff, result, 0, diff);
+            return result;
+        }
+
+        public static T[] RotateByBufferCopy<T>(T[] input, int distance)
+        {
+            // validate
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (distance < 0) throw new ArgumentOutOfRangeException(nameof(distance));
+
+            // rotate
+            var result = new T[input.Length];
+            int diff = distance % input.Length;
+            Buffer.BlockCopy(input, 0, result, diff, input.Length - diff);
+            Buffer.BlockCopy(input, input.Length - diff, result, 0, diff);
+            return result;
+        }
+
+        public static T[] RotateBySpanCopy<T>(T[] input, int distance)
+        {
+            // validate
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (distance < 0) throw new ArgumentOutOfRangeException(nameof(distance));
+
+            // rotate
+            var result = new T[input.Length];
+            var target = new Span<T>(result);
+            var diff = distance % input.Length;
+            var source1 = new Span<T>(input, 0, input.Length - diff);
+            source1.CopyTo(target.Slice(diff, input.Length - diff));
+            var source2 = new Span<T>(input, input.Length - diff, diff);
+            source2.CopyTo(target.Slice(0, diff));
+
+            return result;
+        }
+
+        public unsafe static T[] RotateByUnsafeCopy<T>(T[] input, int distance)
+        {
+            // validate
+            if (input == null) throw new ArgumentNullException(nameof(input));
+            if (distance < 0) throw new ArgumentOutOfRangeException(nameof(distance));
+
+            // rotate
+            var result = new T[input.Length];
+            var size = Marshal.SizeOf<T>();
+            var diff = distance % input.Length;
+            using (var target1 = result.AsMemory(diff, input.Length - diff).Pin())
+            using (var slice1 = input.AsMemory(0, input.Length - diff).Pin())
+            using (var target2 = result.AsMemory(0, diff).Pin())
+            using (var slice2 = input.AsMemory(input.Length - diff, diff).Pin())
+            {
+                void* t1 = target1.Pointer;
+                void* s1 = slice1.Pointer;
+                Unsafe.CopyBlock(t1, s1, (uint)((input.Length - diff) * size));
+                Unsafe.CopyBlock(target2.Pointer, slice2.Pointer, (uint)(diff * size));
+            }
+
             return result;
         }
     }
--- test/Quicker.Tests/CyclicRotationTests.cs
+++ test/Quicker.Tests/CyclicRotationTests.cs
@@ -9,7 +9,11 @@ namespace Quicker.Tests
     {
         [Theory]
         [InlineData(new int[] { 0 }, 0, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 0, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 0, new int[] { 2 })]
         [InlineData(new int[] { 0 }, 1, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 1, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 1, new int[] { 2 })]
         [InlineData(new int[] { 0, 1 }, 1, new int[] { 1, 0 })]
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
@@ -28,7 +32,11 @@ namespace Quicker.Tests
 
         [Theory]
         [InlineData(new int[] { 0 }, 0, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 0, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 0, new int[] { 2 })]
         [InlineData(new int[] { 0 }, 1, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 1, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 1, new int[] { 2 })]
         [InlineData(new int[] { 0, 1 }, 1, new int[] { 1, 0 })]
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
@@ -40,6 +48,75 @@ namespace Quicker.Tests
         {
             // act
             var result = CyclicRotationAlgorithm.RotateByArrayCopy(input, distance);
+
+            // assert
+            Assert.True(result.SequenceEqual(expected));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 0 }, 0, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 0, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 0, new int[] { 2 })]
+        [InlineData(new int[] { 0 }, 1, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 1, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 1, new int[] { 2 })]
+        [InlineData(new int[] { 0, 1 }, 1, new int[] { 1, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 3, new int[] { 3, 4, 5, 0, 1, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 2, 3, 4, 5, 0, 1 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 5, new int[] { 1, 2, 3, 4, 5, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 6, new int[] { 0, 1, 2, 3, 4, 5 })]
+        public void CyclicRotation_RotateByBufferCopy_Returns_Expected_Result(int[] input, int distance, int[] expected)
+        {
+            // act
+            var result = CyclicRotationAlgorithm.RotateByBufferCopy(input, distance);
+
+            // assert
+            Assert.True(result.SequenceEqual(expected));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 0 }, 0, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 0, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 0, new int[] { 2 })]
+        [InlineData(new int[] { 0 }, 1, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 1, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 1, new int[] { 2 })]
+        [InlineData(new int[] { 0, 1 }, 1, new int[] { 1, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 3, new int[] { 3, 4, 5, 0, 1, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 2, 3, 4, 5, 0, 1 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 5, new int[] { 1, 2, 3, 4, 5, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 6, new int[] { 0, 1, 2, 3, 4, 5 })]
+        public void CyclicRotation_RotateBySpanCopy_Returns_Expected_Result(int[] input, int distance, int[] expected)
+        {
+            // act
+            var result = CyclicRotationAlgorithm.RotateBySpanCopy(input, distance);
+
+            // assert
+            Assert.True(result.SequenceEqual(expected));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 0 }, 0, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 0, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 0, new int[] { 2 })]
+        [InlineData(new int[] { 0 }, 1, new int[] { 0 })]
+        [InlineData(new int[] { 1 }, 1, new int[] { 1 })]
+        [InlineData(new int[] { 2 }, 1, new int[] { 2 })]
+        [InlineData(new int[] { 0, 1 }, 1, new int[] { 1, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 1, new int[] { 5, 0, 1, 2, 3, 4 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 2, new int[] { 4, 5, 0, 1, 2, 3 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 3, new int[] { 3, 4, 5, 0, 1, 2 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 2, 3, 4, 5, 0, 1 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 5, new int[] { 1, 2, 3, 4, 5, 0 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 6, new int[] { 0, 1, 2, 3, 4, 5 })]
+        public void CyclicRotation_RotateByUnsafeCopy_Returns_Expected_Result(int[] input, int distance, int[] expected)
+        {
+            // act
+            var result = CyclicRotationAlgorithm.RotateByUnsafeCopy(input, distance);
 
             // assert
             Assert.True(result.SequenceEqual(expected));
